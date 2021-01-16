34c8badd51332f5bf38ddd8ad20ae396adc56316
Stephen Bain <swbain@gmail.com>
2021-01-17 04:34:47 +0700

    added output type selection

--- nest.lua
+++ nest.lua
@@ -7,19 +7,37 @@
 
 selected_output = 1
 
+output_modes = {"lfo", "clock div"}
+
+selected_outputs = {1, 1, 1, 1}
+
 function redraw()
-  local options = {"out 1","out 2","out 3","out 4"}
-  for i = 1,4 do
+  screen.clear()
+  local options = {"out 1", "out 2", "out 3", "out 4"}
+  for i = 1,#options do
     screen.level(selected_output == i and 15 or 3)
-    screen.move(64,12 + (10*i))
+    
+    local y = 12 + 10 * i
+    
+    screen.move (28 ,y)
+    screen.text_center(output_modes[selected_outputs[i]])
+    
+    screen.move(64, y)
     screen.text_center(options[i])
   end
   screen.update()
 end
 
-function key(n,z)
+function key(n, z)
   if n == 2 and z == 1 then
-    selected_output = (selected_output % 4) + 1
+    selected_output = selected_output % 4 + 1
+    redraw()
+  end
+end
+
+function enc(n, d)
+  if n == 2 then
+    selected_outputs[selected_output] = math.min(2, (math.max(selected_outputs[selected_output] + d, 1)))
     redraw()
   end
 end
