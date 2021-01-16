123979dc0b1daf6d5c0ab0853dfda563093b44db
Stephen Bain <swbain@gmail.com>
2021-01-17 04:58:12 +0700

    added output div column/selection

--- nest.lua
+++ nest.lua
@@ -5,11 +5,23 @@
 -- each crow output can be either a clock division/multiplication or a lfo
 -- lfos can be free or clock synced
 
+OUTPUTS = 4
+
 selected_output = 1
 
 output_modes = {"lfo", "clock div"}
 
 selected_output_modes = {1, 1, 1, 1}
+
+div_values = {}
+
+selected_divs = {1, 1, 1, 1}
+
+function init()
+  for i = 1, 32 do
+    div_values[i] = i
+  end
+end
 
 function redraw()
   screen.clear()
@@ -19,11 +31,14 @@ function redraw()
     
     local y = 12 + 10 * i
     
-    screen.move (28 ,y)
+    screen.move (24 ,y)
     screen.text_center(output_modes[selected_output_modes[i]])
     
     screen.move(64, y)
     screen.text_center(options[i])
+    
+    screen.move(104, y)
+    screen.text_center(selected_divs[i])
   end
   screen.update()
 end
@@ -37,7 +52,9 @@ end
 
 function enc(n, d)
   if n == 2 then
-    selected_output_modes[selected_output] = math.min(2, (math.max(selected_output_modes[selected_output] + d, 1)))
-    redraw()
+    selected_output_modes[selected_output] = math.min(#output_modes, (math.max(selected_output_modes[selected_output] + d, 1)))
+  elseif n == 3 then
+    selected_divs[selected_output] = math.min(#div_values, (math.max(selected_divs[selected_output] + d, 1)))
   end
+  redraw()
 end
