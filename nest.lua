06d7d6827ade4aac4e547ce2c4b407a1b7e3243c
Stephen Bain <swbain@gmail.com>
2021-01-17 04:36:28 +0700

    updated variable naming

--- nest.lua
+++ nest.lua
@@ -9,7 +9,7 @@ selected_output = 1
 
 output_modes = {"lfo", "clock div"}
 
-selected_outputs = {1, 1, 1, 1}
+selected_output_modes = {1, 1, 1, 1}
 
 function redraw()
   screen.clear()
@@ -20,7 +20,7 @@ function redraw()
     local y = 12 + 10 * i
     
     screen.move (28 ,y)
-    screen.text_center(output_modes[selected_outputs[i]])
+    screen.text_center(output_modes[selected_output_modes[i]])
     
     screen.move(64, y)
     screen.text_center(options[i])
@@ -37,7 +37,7 @@ end
 
 function enc(n, d)
   if n == 2 then
-    selected_outputs[selected_output] = math.min(2, (math.max(selected_outputs[selected_output] + d, 1)))
+    selected_output_modes[selected_output] = math.min(2, (math.max(selected_output_modes[selected_output] + d, 1)))
     redraw()
   end
 end
