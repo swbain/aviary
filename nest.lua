0d20e3ba752cf4aefae1a1fbaf6f3377cf1defb1
Stephen Bain <swbain@gmail.com>
2021-01-17 04:14:12 +0700

    added output selection

--- nest.lua
+++ nest.lua
@@ -5,12 +5,21 @@
 -- each crow output can be either a clock division/multiplication or a lfo
 -- lfos can be free or clock synced
 
-function init()
-  screen.level(15)
-end
+selected_output = 1
 
 function redraw()
-  screen.move(10, 40)
-  screen.text("hello world")
+  local options = {"out 1","out 2","out 3","out 4"}
+  for i = 1,4 do
+    screen.level(selected_output == i and 15 or 3)
+    screen.move(64,12 + (10*i))
+    screen.text_center(options[i])
+  end
   screen.update()
+end
+
+function key(n,z)
+  if n == 2 and z == 1 then
+    selected_output = (selected_output % 4) + 1
+    redraw()
+  end
 end
