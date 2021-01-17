7547a555fb52e7c74f1b207a209fdf9a03a31465
Stephen Bain <swbain@gmail.com>
2021-01-17 15:12:44 +0700

    cleanup

--- lib/nest.lua
+++ lib/nest.lua
@@ -1,26 +1,15 @@
 local nest = {}
 
 local OUTPUTS = 4
-
 local GATE_ACTION = "{to(5,0),to(0,0.05)}"
 
 local selected_output = 1
-
 local output_modes = {"clock div", "lfo"}
-
 local selected_output_modes = {1, 1, 1, 1}
-
-local div_values = {}
-
 local selected_divs = {1, 1, 1, 1}
-
 local clock_ids = {}
 
 function nest.init()
-  for i = 1, 32 do
-    div_values[i] = i
-  end
-  
   init_crow()
   init_clock()
 end
@@ -48,15 +37,15 @@ end
 function nest.key(n, z)
   if n == 2 and z == 1 then
     selected_output = selected_output % 4 + 1
-    redraw()
   end
+  redraw()
 end
 
 function nest.enc(n, d)
   if n == 2 then
     selected_output_modes[selected_output] = math.min(#output_modes, (math.max(selected_output_modes[selected_output] + d, 1)))
   elseif n == 3 then
-    selected_divs[selected_output] = math.min(#div_values, (math.max(selected_divs[selected_output] + d, 1)))
+    selected_divs[selected_output] = math.min(32, (math.max(selected_divs[selected_output] + d, 1)))
   end
   update_crow_action()
   clock.cancel(clock_ids[selected_output])
